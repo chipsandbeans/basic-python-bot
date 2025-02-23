0e5b1a54ba836bd8a54dfad814d5a6bc44677838
chipsandbeans <bryanmurphy401@gmail.com>
2025-02-23 13:36:51 +0000

    Built a basic Python chatbot

---
+++ script.py
@@ -0,0 +1,11 @@
+print("Hello! What is your name?")
+name = input("Hello! What is your name? ")
+bot_name = "Friendly Bot"
+print(f"Nice to meet you, {name}! My name is {bot_name}")
+age_input = input(f"How old are you, {name}? ")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color = input("What's your favorite color? ")
+print(f"Oh, {color} is a beautiful color!")
